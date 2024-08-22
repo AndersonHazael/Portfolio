48e5c3b54774f2dd8904ead05d0af9971bcb9458
AndersonHazael <117550667+AndersonHazael@users.noreply.github.com>
2024-08-22 16:21:55 -0300

    Update btn.js

--- js/utils/btn.js
+++ js/utils/btn.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function(){
 
     let btnLink = document.getElementById('btnLinkedin');
     btnLink.addEventListener('click', function(){
-        window.open('https://www.linkedin.com/in/anderson-ashael-654584165', '_blank');
+        window.open('https://www.linkedin.com/in/anderson-ashael/', '_blank');
     });
 
 })
